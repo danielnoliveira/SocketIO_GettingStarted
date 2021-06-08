c1ac74de9635749cbc452e3be6b2a14c3f025168
Daniel Nogueira de Oliveira <daniel.n.oliveira@outlook.com>
2021-06-08 12:17:46 -0300

    Tudo que ja fiz. so falta adicionar mensagens privadas

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+const users = {};
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname+'/index.html');
+});
+
+io.on('connect',socket => {
+  console.log('a user conected: ',socket.id);
+  socket.on('disconnect',()=>{
+    socket.broadcast.emit('chat message','system',`${users[socket.id]} disconnected from chat`,'d');
+    delete users[socket.id];
+    io.emit('users connected',Object.values(users));
+  });
+  socket.on('my nickname',nickname => {
+    users[socket.id] = nickname;
+    io.emit('users connected',Object.values(users));
+    socket.broadcast.emit('chat message','system',`${users[socket.id]} connected to chat`,'c');
+  });
+  socket.on('user is typing',user => {
+    socket.broadcast.emit('someone is typing',user);
+  });
+
+  socket.on('chat message', (msg) => {
+    //socket.to(`room#${socket_id}`).emit('chat message',users[socket.id],msg);
+    socket.broadcast.emit('chat message', users[socket.id] ,msg);
+  });
+});
+
+server.listen(3000,()=>{
+  console.log('Server running!!!!');
+});
